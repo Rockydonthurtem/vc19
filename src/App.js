cca77a5b54e83356b3406cb66bbf0fb1f569b618
Cedric Harris <cedricharris@Cedrics-Air.home>
2020-04-19 16:53:53 -0400

    0

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import React, { Component } from 'react'
+
+export default class App extends Component {
+    render() {
+        return (
+            <div>
+                <h1>HI</h1>
+            </div>
+        )
+    }
+}
