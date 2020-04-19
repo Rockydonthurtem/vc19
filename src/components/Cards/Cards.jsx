920553751452ed6074df00f4be99e5624d8de74f
Cedric Harris <cedricharris@Cedrics-Air.home>
2020-04-19 18:36:49 -0400

    status

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,55 +1,74 @@
 import React from 'react'
 import {Card, CardContent, Typography, Grid} from '@material-ui/core'
+import Countup from 'react-countup'
+import cx from 'classnames'
 
 import styles from './Cards.module.css'
 
- const Cards = (props) => {
-     console.log("PROS",props)
+ const Cards = ({data: {confirmed, recovered, deaths, lastUpdate}}) => {
+     if(!confirmed){
+         return "Loading...";
+     }
     return (
         <div className={styles.container} >
             <Grid container spacing={3} justify="center">
-                <Grid item component={Card}>
+                <Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.infected)}>
                     <CardContent>
                         <Typography color="textSecondary" gutterBottom>
                             Infected
                         </Typography>
                         <Typography variant="h5">
-                            Real data
+                          <Countup 
+                           start={0}
+                           end={confirmed.value}
+                           duration={2.5}
+                           separator=","
+                          />
                         </Typography>
                         <Typography color="textSecondary" >
-                            Real date
+                           {new Date(lastUpdate).toDateString()}
                         </Typography>
                         <Typography variant="body2">
                             Number of active
                         </Typography>
                     </CardContent>
                 </Grid>
-                <Grid item component={Card}>
+                <Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.recovered)}>
                     <CardContent>
                         <Typography color="textSecondary" gutterBottom>
                             Recovered
                         </Typography>
-                        <Typography variant="h5">
-                            Real data
+                       <Typography variant="h5">
+                          <Countup 
+                           start={0}
+                           end={recovered.value}
+                           duration={2.5}
+                           separator=","
+                          />
                         </Typography>
-                        <Typography color="textSecondary" >
-                            Real date
+                         <Typography color="textSecondary" >
+                           {new Date(lastUpdate).toDateString()}
                         </Typography>
                         <Typography variant="body2">
                             Number of active
                         </Typography>
                     </CardContent>
                 </Grid>
-                <Grid item component={Card}>
+                <Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.deaths)}>
                     <CardContent>
                         <Typography color="textSecondary" gutterBottom>
                             Deaths
                         </Typography>
-                        <Typography variant="h5">
-                            Real data
+                       <Typography variant="h5">
+                          <Countup 
+                           start={0}
+                           end={deaths.value}
+                           duration={2.5}
+                           separator=","
+                          />
                         </Typography>
-                        <Typography color="textSecondary" >
-                            Real date
+                         <Typography color="textSecondary" >
+                           {new Date(lastUpdate).toDateString()}
                         </Typography>
                         <Typography variant="body2">
                             Number of active
